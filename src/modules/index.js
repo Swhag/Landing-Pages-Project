4a2740a146af0309e0ed72b5d64fd31eb8c0bf90
Swhag <khg5293@outlook.com>
2023-01-28 23:30:39 -0500

    configured webpack environment

---
+++ src/modules/index.js
@@ -0,0 +1,7 @@
+import '../styles/main.css';
+// import '../styles/apps.css';
+
+import hero from '../img/Home/hero.jpg';
+import gallery1 from '../img/Home/gallery1.jpg';
+import gallery2 from '../img/Home/gallery2.jpg';
+import gallery3 from '../img/Home/gallery3.jpg';
